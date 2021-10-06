70146a6787a110267ef95b10229da13ced3c1e2f
luisprofessor <luisprofessor@gmail.com>
2021-10-06 11:52:55 -0300

    Initial commit

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+interface Componente{
+  icon:string,
+  name:string,
+  redirectTo:string
+}
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+rutas:Componente[]=[
+
+  {
+    icon:'caret-back-outline',
+    name:'Alert',
+    redirectTo:'/alert'
+  },
+  {
+    icon:'caret-back-outline',
+    name:'Action-Sheet',
+    redirectTo:'/action-sheet'
+  }
+
+]
+
+
+  // rutas:any[]=[
+  //   {
+  //     titulo:'Alert',
+  //     ruta:'/alert'
+  //   },
+  //   {
+  //     titulo:'Action-Sheet',
+  //     ruta:'/action-sheet'
+  //   }
+  // ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
